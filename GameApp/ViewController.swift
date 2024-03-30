453814455946c810d761c840ed394b0d0090fa03
naelafauzul <naelafauzulm@gmail.com>
2024-03-30 13:33:12 +0700

    Initial Commit

---
+++ GameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameApp
+//
+//  Created by Naela Fauzul Muna on 30/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
